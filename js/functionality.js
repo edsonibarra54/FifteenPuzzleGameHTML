c1b8a0a2243974f2fb3f42e51b1afd4006b3a0e8
Edson Ibarra <edsonibarra54@gmail.com>
2023-09-27 18:16:51 -0600

    Added functionality to move the numbers

--- js/functionality.js
+++ js/functionality.js
@@ -7,14 +7,15 @@ values.push(0);
 var name;
 var timer;
 var seconds = 0;
+var moves = 0;
 
 function begin(){
     var randomIndex;
     var value;
     var divElement;
 
-    name = prompt("Whats your name?")
-    document.getElementsByClassName("name")[0].innerHTML = name;
+    //name = prompt("Whats your name?")
+    //document.getElementsByClassName("name")[0].innerHTML = name;
 
     //Creating board
     board = new Array(tam);
@@ -51,4 +52,75 @@ function printBoard(){
     for(var i = 0 ; i < tam ; i++){
         console.log(board[i])
     }
+}
+
+function moveBox(posI, posJ){
+    var divElement;
+    var divNextElement;
+    var i = parseInt(posI);
+    var j = parseInt(posJ);
+    
+    //Top
+    if(j - 1 >= 0)
+        if(board[i][j-1] == 0){
+            divElement = document.getElementById(posI+posJ);
+            divNextElement = document.getElementById(i.toString()+(j-1).toString())
+            board[i][j-1] = board[i][j]; 
+            board[i][j] = 0;
+            document.getElementById(posI+posJ).innerHTML = board[i][j];
+            document.getElementById(i.toString()+(j-1).toString()).innerHTML = board[i][j-1];
+            divNextElement.style.backgroundColor = "#353435";
+            divElement.style.backgroundColor = "white";
+
+            moves++;
+            document.getElementsByClassName("moves")[0].innerHTML = moves;
+        }
+    
+    //Bottom
+    if(j + 1 < tam)
+        if(board[i][j+1] == 0){
+            divElement = document.getElementById(posI+posJ);
+            divNextElement = document.getElementById(i.toString()+(j+1).toString())
+            board[i][j+1] = board[i][j]; 
+            board[i][j] = 0;
+            document.getElementById(posI+posJ).innerHTML = board[i][j];
+            document.getElementById(i.toString()+(j+1).toString()).innerHTML = board[i][j+1];
+            divNextElement.style.backgroundColor = "#353435";
+            divElement.style.backgroundColor = "white";
+
+            moves++;
+            document.getElementsByClassName("moves")[0].innerHTML = moves;
+        }
+
+    //Left
+    if(i - 1 >= 0)
+        if(board[i-1][j] == 0){
+            divElement = document.getElementById(posI+posJ);
+            divNextElement = document.getElementById((i-1).toString()+j.toString())
+            board[i-1][j] = board[i][j]; 
+            board[i][j] = 0;
+            document.getElementById(posI+posJ).innerHTML = board[i][j];
+            document.getElementById((i-1).toString()+j.toString()).innerHTML = board[i-1][j];
+            divNextElement.style.backgroundColor = "#353435";
+            divElement.style.backgroundColor = "white";
+
+            moves++;
+            document.getElementsByClassName("moves")[0].innerHTML = moves;
+        }
+
+    //Right
+    if(i + 1 < tam)
+        if(board[i+1][j] == 0){
+            divElement = document.getElementById(posI+posJ);
+            divNextElement = document.getElementById((i+1).toString()+j.toString())
+            board[i+1][j] = board[i][j]; 
+            board[i][j] = 0;
+            document.getElementById(posI+posJ).innerHTML = board[i][j];
+            document.getElementById((i+1).toString()+j.toString()).innerHTML = board[i+1][j];
+            divNextElement.style.backgroundColor = "#353435";
+            divElement.style.backgroundColor = "white";
+
+            moves++;
+            document.getElementsByClassName("moves")[0].innerHTML = moves;
+        }
 }
